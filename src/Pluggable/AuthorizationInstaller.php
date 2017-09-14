7ab6b5db3c72078755497d93b2e7f602d4e5aaad
Axel Rutz <axel.rutz@clever-systems.net>
2017-09-14 04:15:45 +0200

    Worked around a bug in symfony fs

--- src/Pluggable/AuthorizationInstaller.php
+++ src/Pluggable/AuthorizationInstaller.php
@@ -17,7 +17,8 @@ class AuthorizationInstaller {
     $fs = new Filesystem();
     if (
       is_link($dir)
-      && ($linkTarget = $fs->readlink($dir, TRUE))
+      // @todo Upstream this forked method.
+      && ($linkTarget = $this->readlink($dir, TRUE))
       && !file_exists($linkTarget)
     ) {
       $fs->mkdir($linkTarget);
@@ -47,6 +48,46 @@ class AuthorizationInstaller {
     $fileContent = $extractor->getCheckContent($authorizationChallenge);
     $basename = basename($url);
     return array($fileContent, $basename);
+  }
+
+  /**
+   * Resolves links in paths.
+   *
+   * With $canonicalize = false (default)
+   *      - if $path does not exist or is not a link, returns null
+   *      - if $path is a link, returns the next direct target of the link without considering the existence of the target
+   *
+   * With $canonicalize = true
+   *      - if $path does not exist, returns null
+   *      - if $path exists, returns its absolute fully resolved final version
+   *
+   * @param string $path         A filesystem path
+   * @param bool   $canonicalize Whether or not to return a canonicalized path
+   *
+   * @return string|null
+   */
+  public function readlink($path, $canonicalize = false)
+  {
+    $fs = new Filesystem();
+    if (!$canonicalize && !is_link($path)) {
+      return;
+    }
+
+    if ($canonicalize) {
+      if (!$fs->exists($path)) {
+        return;
+      }
+
+      $dir = dirname($path);
+      $path = readlink($path);
+
+      if (!$fs->isAbsolutePath($path)) {
+        $path = $dir . DIRECTORY_SEPARATOR . $path;
+      }
+      return realpath($path);
+    }
+
+    return readlink($path);
   }
 
 }
